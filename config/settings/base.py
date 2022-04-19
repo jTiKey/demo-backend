e7ee3c0881ed16aab6f60da221a688e62e2f3c1d
Slava <slava.khromyak@gmail.com>
2022-04-19 03:51:09 +0300

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -227,7 +227,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Viacheslav Khromiak""", "slava.khromyak@gmail.com")]
+ADMINS = [("""slava""", "demo@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
